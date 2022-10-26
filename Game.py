c05f6b5e9df68422bf79672ef2cfda21b909b0c8
Harambe2Point0 <gamerboy2point0@gmail.com>
2022-10-26 16:08:17 -0700

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,12 +1,28 @@
 # Setup
-
 print ("Setup Initiated")
 
-g = input ("Enter Word to be Guessed")
+g = input("Enter Word to be Guessed")
+array = []
+correct = False
 
-print ("This is Hangman! You will have 5 guesses to spell the word. If you do not spell the word within 5 guesses, he dies.")
+for i in range(0, len(g)):
+    array.append("_")
+
+print ("This is Hangman! You will have 7 guesses to spell the word. If you do not spell the word within 5 guesses, he dies.")
 print ("\n"*40)
 
-# Logic
 
-input ("First Guess")
+# Logic
+while True:
+    for i in range (0, len(g)):
+        print(array[i], end="")
+    print()
+    guess = input("First Guess")
+    for i in range(0, len(g)):
+        if guess == g[i]:
+            correct = True
+            array[i] = guess
+    if correct == True:
+        print ("Correct")
+    else:
+        print ("Incorrect, Nerd")
