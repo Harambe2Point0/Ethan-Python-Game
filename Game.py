5107ba495e27d1dff0805ba88b29e3e9dc72d011
Harambe2Point0 <gamerboy2point0@gmail.com>
2022-10-26 17:06:13 -0700

    Update Game.py

--- Game.py
+++ Game.py
@@ -7,7 +7,6 @@ array = []
 for i in range(0, len(g)):
     array.append("_")
 
-print ("This is Hangman! You will have 7 guesses to spell the word. If you do not spell the word within 5 guesses, he dies.")
 print ("\n"*40)
 
 
@@ -18,11 +17,14 @@ while True:
         print(array[i], end="")
     print()
     guess = input()
-    for i in range(0, len(g)):
-        if guess == g[i]:
-            correct = True
-            array[i] = guess
-    if correct == True:
-        print ("Correct")
+    if len(guess) < 2:
+        for i in range(0, len(g)):
+            if guess == g[i]:
+                correct = True
+                array[i] = guess
+        if correct == True:
+            print ("Correct")
+        else:
+            print ("Incorrect")
     else:
-        print ("Incorrect, Nerd")
+        print ("U suck. Put in one letter")
